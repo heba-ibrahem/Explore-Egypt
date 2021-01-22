7857724503776bb3721c5cc9a85e1e621b09b134
asmaaelqady <as2161996>
2021-01-22 22:01:39 +0200

    add home component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Component/home/home.component';
+import { EditProfileComponent } from './Component/edit-profile/edit-profile.component';
+import { AdminPanelComponent } from './Component/Admin/admin-panel/admin-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EditProfileComponent,
+    AdminPanelComponent
   ],
   imports: [
     BrowserModule,
