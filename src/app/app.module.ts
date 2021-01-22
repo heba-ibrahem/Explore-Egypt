a0b6e6899e2c4cbf59d8d4b9704cb24364e7932a
asmaaelqady <as2161996>
2021-01-22 22:40:38 +0200

    edit component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './Component/home/home.component';
-import { EditProfileComponent } from './Component/edit-profile/edit-profile.component';
-import { AdminPanelComponent } from './Component/Admin/admin-panel/admin-panel.component';
+import { HomeComponent } from './components/home/home.component';
+import { EditProfileComponent } from './components/edit-profile/edit-profile.component';
+import { AdminPanelComponent } from './components/Admin/admin-panel/admin-panel.component';
 
 @NgModule({
   declarations: [
