071288d3f87d4cb19909ac98ac6324c75c6ad70d
Heba-Ibrahem <pt.heba.ibrahem@gmail.com>
2021-01-22 22:09:23 +0200

    adding compontants and db files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './componants/login/login.component';
+import { RegisterComponent } from './componants/register/register.component';
+import { PlanProgramComponent } from './componants/plan-program/plan-program.component';
+import { DesginProgramComponent } from './componants/desgin-program/desgin-program.component';
+import { ProgramDetailsComponent } from './componants/program-details/program-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    PlanProgramComponent,
+    DesginProgramComponent,
+    ProgramDetailsComponent
   ],
   imports: [
     BrowserModule,
