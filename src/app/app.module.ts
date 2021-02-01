529be1ba6e446bb9d93dc80c1b562769f0a37786
Khaled-Fag-elnour <a.hassan@wasiladev.com>
2021-02-01 08:21:35 +0200

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,7 +15,7 @@ import { RegisterComponent } from './components/register/register.component';
 import { PlanProgramComponent } from './components/plan-program/plan-program.component';
 import { DesginProgramComponent } from './components/desgin-program/desgin-program.component';
 import { ProgramDetailsComponent } from './components/program-details/program-details.component';
-import { HomeComponent } from './components/Home/home/home.component';
+import { HomeComponent } from './components/home/home/home.component';
 import { EditProfileComponent } from './components/edit-profile/edit-profile.component';
 import { AdminPanelComponent } from './components/Admin/admin-panel/admin-panel.component';
 import { ActivitiesComponent } from './components/activities/activities.component';
@@ -23,9 +23,9 @@ import { ArticleComponent } from './components/article/article.component';
 import { TrendingComponent } from './components/trending/trending.component';
 import { NotFoundComponent } from './components/not-found/not-found.component';
 import { HttpClientModule } from '@angular/common/http';
-import { PlanTripHomeComponent } from './components/Home/plan-trip-home/plan-trip-home.component';
-import { ActivitiesHomeComponent } from './components/Home/activities-home/activities-home.component';
-import { ExplorEgyptHomeComponent } from './components/Home/explor-egypt-home/explor-egypt-home.component';
+import { PlanTripHomeComponent } from './components/home/plan-trip-home/plan-trip-home.component';
+import { ActivitiesHomeComponent } from './components/home/activities-home/activities-home.component';
+import { ExplorEgyptHomeComponent } from './components/home/explor-egypt-home/explor-egypt-home.component';
 
 
 @NgModule({
