0f215bee5f54995efb28ad22d9d5b4587e8200a3
Khaled-Fag-elnour <a.hassan@wasiladev.com>
2021-03-10 15:31:03 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
 import { LocalizationService } from './Services/localization.service';
+
 
 @Component({
   selector: 'app-root',
@@ -9,7 +11,7 @@ import { LocalizationService } from './Services/localization.service';
 export class AppComponent {
   title = 'ExploreEgypt';
   currentLang: string = 'en';
-  constructor(private localizationService: LocalizationService) {
+  constructor(private localizationService: LocalizationService, @Inject(DOCUMENT) private document: Document) {
     this.currentLang = this.localizationService.getCurrentLang();
     this.loadStyles();
   }
@@ -18,12 +20,43 @@ export class AppComponent {
     document.documentElement.setAttribute('lang', this.currentLang);
     console.log(this.currentLang);
     
-    if (this.currentLang === 'ar') {
+    if (this.currentLang == 'ar') {
       document.body.classList.add('rtl');
+      this.loadArStyles();
       // require("style-loader!../assets/css/bootstrap-rtl.min.css");
-      // require("style-loader!../../src/assets/css/rtl-fixes.css");
+      // require("style-loader!../assets/css/rtl-fixes.css");
     } else {
       // require("style-loader!../assets/css/bootstrap.min.css")
     }
   }
+
+
+  loadArStyles() {
+    let headTag = this.document.getElementsByTagName("head")[0] as HTMLHeadElement;
+    // let existingLink = this.document.getElementById("arCss") as HTMLLinkElement;
+    let rtlFixesBundleName = "StyleAr.css";
+    let bootstrapRtlBundleName = "bootstrapAr.css";
+
+    // rtl-fixes.css
+    let newLink = this.document.createElement("link");
+    newLink.rel = "stylesheet";
+    newLink.type = "text/css";
+    newLink.id = "arCss";
+    newLink.href = rtlFixesBundleName;
+    headTag.appendChild(newLink);
+    
+    // bootstrap-rtl.min.css
+    let bootstrapRtlLink = this.document.createElement("link");
+    bootstrapRtlLink.rel = "stylesheet";
+    bootstrapRtlLink.type = "text/css";
+    bootstrapRtlLink.id = "arCss";
+    bootstrapRtlLink.href = bootstrapRtlBundleName;
+    headTag.appendChild(bootstrapRtlLink);
+  }
+
+
+
+
 }
+
+
