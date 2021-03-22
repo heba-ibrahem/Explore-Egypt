64ed7df7a8e79e81ba551aa23fbbe05b976825f2
Heba-Ibrahem <pt.heba.ibrahem@gmail.com>
2021-03-22 19:10:42 +0200

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/explore-egypt'));
+app.use(express.static(__dirname + '/dist/ExploreEgypt'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/explore-egypt/index.html'));
+res.sendFile(path.join(__dirname+'/dist/ExploreEgypt/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
